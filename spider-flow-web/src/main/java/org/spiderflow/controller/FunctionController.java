5ebe9dc918cda60cad08418c2c7b4427646e07bd
chanson <1527540881@qq.com>
2024-04-06 00:43:00 +0800

    添加备注信息

--- spider-flow-web/src/main/java/org/spiderflow/controller/FunctionController.java
+++ spider-flow-web/src/main/java/org/spiderflow/controller/FunctionController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 基础功能处理器
+ */
 @RestController
 @RequestMapping("/function")
 public class FunctionController {
@@ -19,6 +22,13 @@ public class FunctionController {
     @Autowired
     private FunctionService functionService;
 
+    /**
+     * 查询所有爬虫任务 list返回
+     * @param page
+     * @param size
+     * @param name
+     * @return
+     */
     @RequestMapping("/list")
     public IPage<Function> list(@RequestParam(name = "page",defaultValue = "1")Integer page, @RequestParam(name = "limit",defaultValue = "1")Integer size,String name) {
         QueryWrapper<Function> select = new QueryWrapper<Function>().select("id", "name", "parameter", "create_date");
@@ -29,16 +39,30 @@ public class FunctionController {
         return functionService.page(new Page<Function>(page, size), select);
     }
 
+    /**
+     * 保存
+     * @param function
+     * @return
+     */
     @RequestMapping("/save")
     public String save(Function function){
         return functionService.saveFunction(function);
     }
 
+    /**
+     * 基于特定的id查询详情
+     * @param id
+     * @return
+     */
     @RequestMapping("/get")
     public Function get(String id){
         return functionService.getById(id);
     }
 
+    /**
+     * 基于某个id删除
+     * @param id
+     */
     @RequestMapping("/remove")
     public void remove(String id){
         functionService.removeById(id);
